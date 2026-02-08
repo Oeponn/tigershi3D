2bb6966ae455ce266c00aae17685c41b4d22344a
Tiger Shi <tigershi0110@gmail.com>
2026-02-07 23:03:40 -0500

    adding typed scss generator and concurrently

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,19 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react({
       babel: {
-        plugins: [['babel-plugin-react-compiler']],
+        plugins: [["babel-plugin-react-compiler"]],
       },
     }),
   ],
-})
+  css: {
+    modules: {
+      // Classes like progress-bar will be transformed to progressBar
+      localsConvention: "camelCaseOnly",
+    },
+  },
+});
